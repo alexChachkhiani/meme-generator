767dc4b2319ad55e92a9e96f7c2901ca4f9e82c3
alexOrdnas <devfterr@gmail.com>
2024-04-02 20:05:19 +0400

    add: minor modification for DEAR MATEgit statusgit status!

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className="header">
             <img src={TrollFace} alt="Troll Face" className="header--image"/>
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">React Course - Project 3 </h4>
         </div>
     </div>
   )
